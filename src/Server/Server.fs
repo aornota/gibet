06cbf3cba593fe5e7da2793542c348cf38da0f1f
aornota <djnarration@gmail>
2019-04-07 18:44:51 +0100

    First commit: using SAFE template and Fable 3.0 prerelease (&c.)

---
+++ src/Server/Server.fs
@@ -0,0 +1,54 @@
+open System
+open System.IO
+open System.Threading.Tasks
+
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.Extensions.DependencyInjection
+
+open FSharp.Control.Tasks.V2
+open Giraffe
+open Shared
+
+open Fable.Remoting.Server
+open Fable.Remoting.Giraffe
+open Microsoft.WindowsAzure.Storage
+
+let tryGetEnv = System.Environment.GetEnvironmentVariable >> function null | "" -> None | x -> Some x
+
+let publicPath = tryGetEnv "public_path" |> Option.defaultValue "../Client/public" |> Path.GetFullPath
+let storageAccount = tryGetEnv "STORAGE_CONNECTIONSTRING" |> Option.defaultValue "UseDevelopmentStorage=true" |> CloudStorageAccount.Parse
+let port = "SERVER_PORT" |> tryGetEnv |> Option.map uint16 |> Option.defaultValue 8085us
+
+let getInitCounter () : Task<Counter> = task { return { Value = 42 } }
+
+let counterApi = {
+    initialCounter = getInitCounter >> Async.AwaitTask
+}
+
+let webApp =
+    Remoting.createApi()
+    |> Remoting.withRouteBuilder Route.builder
+    |> Remoting.fromValue counterApi
+    |> Remoting.buildHttpHandler
+
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseDefaultFiles()
+       .UseStaticFiles()
+       .UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    services.AddGiraffe() |> ignore
+    tryGetEnv "APPINSIGHTS_INSTRUMENTATIONKEY" |> Option.iter (services.AddApplicationInsightsTelemetry >> ignore)
+
+WebHost
+    .CreateDefaultBuilder()
+    .UseWebRoot(publicPath)
+    .UseContentRoot(publicPath)
+    .Configure(Action<IApplicationBuilder> configureApp)
+    .ConfigureServices(configureServices)
+    .UseUrls("http://0.0.0.0:" + port.ToString() + "/")
+    .Build()
+    .Run()
