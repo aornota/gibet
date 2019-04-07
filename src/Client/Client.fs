e00be9bed2be779be0627cf70e129c7ea8e59029
aornota <djnarration@gmail>
2019-04-07 23:10:19 +0100

    More tweaks

--- src/Client/Client.fs
+++ src/Client/Client.fs
@@ -103,7 +103,7 @@ let view (model : Model) (dispatch : Msg -> unit) =
         [ Navbar.navbar [ Navbar.Color IsPrimary ]
             [ Navbar.Item.div [ ]
                 [ Heading.h2 [ ]
-                    [ str "SAFE Template" ] ] ]
+                    [ str "gibet (α)" ] ] ]
 
           Container.container []
               [ Content.content [ Content.Modifiers [ Modifier.TextAlignment (Screen.All, TextAlignment.Centered) ] ]
